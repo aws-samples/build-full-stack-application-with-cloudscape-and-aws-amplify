130b6916bcee6ebbe6a658f14d48062f019340cb
yongzhe525 <renyz0525@naver.com>
2023-10-26 23:15:10 +0900

    modify init.sh

--- script/init/init.sh
+++ script/init/init.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
+# Get the region information
+current_region=$(aws configure get region)
 
-# CloudFormation 스택의 이름 설정
+# Get the name of cloudFormation stack
 stack_name="base-cfn"
 
-# 스택 출력 값을 가져오기
+# Get the access values from cloudFormation stack
 AccessKey=$(aws cloudformation describe-stacks --stack-name $stack_name --query "Stacks[0].Outputs[?OutputKey=='AccessKey'].OutputValue" --output text)
 SecretKey=$(aws cloudformation describe-stacks --stack-name $stack_name --query "Stacks[0].Outputs[?OutputKey=='SecretKey'].OutputValue" --output text)
 
-# 출력 값 확인
+# check the output
 echo "AccessKey Value: $AccessKey"
 echo "SecretKey Value: $SecretKey"
-
 
 combined_output="AccessKey: $AccessKey\nSecretKey: $SecretKey"
 echo -e $combined_output > amplify_config.txt
 
+# Download the web applicaion from Gitlab
+git clone -b $1 --depth 1 https://github.com/aws-samples/build-full-stack-application-with-cloudscape-and-aws-amplify
+
+# Install libraries
+cd build-full-stack-application-with-cloudscape-and-aws-amplify
+
 npm install -g @aws-amplify/cli
 sudo yum -y install xdg-utils
-
 npm install reactstrap --force
